72a778ea0f70f3d2f027e1dd5c1a8404f9503339
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-01 22:33:10 +0300

    Added profile screen to the route

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom"
 import Login from "../Screens/Login"
 import Register from "../Screens/Register"
 import Verification from "../Screens/Verification"
+import Profile from "../Screens/Profile"
 
 export default function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verification" element={<Verification />} />
         <Route path="*" element={<div>404 - Page Not Found</div>} />
