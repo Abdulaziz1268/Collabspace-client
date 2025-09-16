441aee138061ebdcf72384b25833795b71a92bcf
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-16 03:00:48 +0300

    Refactor Feed component to fetch posts from API and improve loading state handling

--- src/Screens/Feed.jsx
+++ src/Screens/Feed.jsx
@@ -1,16 +1,51 @@
+import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+
+import { apiWithUserAuth } from "../Config/Api"
 import FeedCard from "../Components/FeedCard"
-import { Data } from "../Config/feedData"
+import placeholder from "../assets/placeholder.jpg"
+import { UserContext } from "../Context/User"
 
 export default function Feed() {
+  const [data, setData] = useState([])
+  const [postLoading, setPostLoading] = useState(false)
   const navigate = useNavigate()
+  const { userData, loading } = useContext(UserContext)
+
+  useEffect(() => {
+    setPostLoading(true)
+    const fetchFeeds = async () => {
+      try {
+        const api = apiWithUserAuth()
+        const response = await api.get("/api/post/getPosts")
+        setData(response.data)
+      } catch (error) {
+        console.log(error.response?.data?.message || error.message)
+      } finally {
+        setTimeout(() => {
+          setPostLoading(false)
+        }, 5000)
+      }
+    }
+
+    fetchFeeds()
+  }, [])
+  console.log(data)
 
   return (
-    <div className="w-full flex justify-center">
-      <div className="flex flex-col justify-center items-center shadow-2xl sm:w-4/6">
-        <div className="w-full px-6 flex items-center">
+    <div className="w-full flex justify-center md:justify-end lg:justify-center">
+      <div className="flex flex-col justify-center items-center shadow-2xl md:w-2/3 lg:w-3/6">
+        <div
+          className={`w-full px-3 flex items-center bg-white ${
+            loading && "animate-pulse"
+          }`}
+        >
           <img
-            src={Data[2].authorImage}
+            src={
+              !loading
+                ? `http://localhost:2005${userData.imageUrl}`
+                : placeholder
+            }
             className="w-15 h-15 m-2 rounded-full border border-gray-600"
           />
           <p
@@ -21,8 +56,8 @@ export default function Feed() {
           </p>
         </div>
         <div className="bg-gray-400 w-full">
-          {Data.map((feed, index) => (
-            <FeedCard feed={feed} key={index} />
+          {data.map((feed, index) => (
+            <FeedCard feed={feed} key={index} postLoading={postLoading} />
           ))}
         </div>
       </div>
